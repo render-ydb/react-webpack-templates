d475dcafbe247e69af30ff1778562cf8323041a3
yangdaobo.ydb <yangdaobo.ydb@alibaba-inc.com>
2024-03-20 14:38:28 +0800

    fix: 😅 change template content

---
+++ packages/react-app-template/template/src/pages/Test/index.tsx
@@ -0,0 +1,11 @@
+import View from "@x.render/render-view";
+import Text from "@x.render/render-text";
+const TestPage = () => {
+  return (
+    <View>
+      <Text>TestPage</Text>
+    </View>
+  );
+};
+
+export default TestPage;
